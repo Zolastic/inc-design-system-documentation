9b5d533756e1bfd0feb624e0613d639c9581e114
Zolastic <honhattienlul@gmail.com>
2024-04-25 01:26:41 +0800

    fix(docs/layout.tsx): fix bug where SiteHeader would disappear when switching from homepage to docs pages
    - The issue was due to improper nesting of HTML elements. The root layout had a 'body' element wrapping around the children, which is not allowed. Similarly, the 'body' element cannot be a child of another 'body' element. The original layout for docs had an 'html' element wrapped around it, which caused a warning/error. This commit fixes these issues by correcting the HTML structure.

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -4,15 +4,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="flex-1">
-        <div className="border-b bg-background">
-          <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
-            <div></div>
-            <div>{children}</div>
-          </div>
+    <div className="flex-1">
+      <div className="border-b bg-background">
+        <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
+          <div></div>
+          {children}
         </div>
-      </body>
-    </html>
+      </div>
+    </div>
   );
 }
