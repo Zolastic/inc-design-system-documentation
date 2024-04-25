8dbcc9b105de843de6ca85c135121191a8405e72
Zolastic <honhattienlul@gmail.com>
2024-04-25 21:25:25 +0800

    feat: add code example and copy code feature for Select component page

--- src/app/docs/components/select/page.tsx
+++ src/app/docs/components/select/page.tsx
@@ -3,20 +3,21 @@
 import {
   Select,
   SelectContent,
-  SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "inc-design-system/select";
-import React, { useState } from "react";
+import { Check, Clipboard } from "lucide-react";
+import { useState } from "react";
+import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
+import { github } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 type Props = {};
 
 const Page = (props: Props) => {
-  const [showCode, setShowCode] = useState(false);
-  const code = `
-  import {
+  const [copy, setCopy] = useState(false);
+
+  const code = `import {
     Select,
     SelectContent,
     SelectItem,
@@ -40,8 +41,7 @@ const Page = (props: Props) => {
     );
   };
   
-  export default SelectDemo;  
-  `;
+  export default SelectDemo;`;
 
   return (
     <>
@@ -49,26 +49,54 @@ const Page = (props: Props) => {
       <p className="text-lg text-text-default/60 max-w-[750px] mt-2">
         Select component to select value from options.
       </p>
-      <div className="mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 relative rounded-md border">
+      <div className="mt-4 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 relative rounded-md border">
         <div className="preview flex min-h-[350px] w-full justify-center p-10 items-center overflow-auto">
-          {!showCode && (
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Theme" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="light">Light</SelectItem>
-                <SelectItem value="dark">Dark</SelectItem>
-                <SelectItem value="system">System</SelectItem>
-              </SelectContent>
-            </Select>
-          )}
-          {showCode && (
-            <pre>
-              <code>{code}</code>
-            </pre>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Theme" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">Light</SelectItem>
+              <SelectItem value="dark">Dark</SelectItem>
+              <SelectItem value="system">System</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      <div className="max-w-21 min-w-[25rem] bg-grey-100 rounded-md overflow-hidden">
+        <div className="flex justify-between px-4 text-text-default text-xs items-center">
+          <p className="text-sm">Example Code</p>
+          {copy ? (
+            <button className="py-1 inline-flex items-center gap-1">
+              <Check size={16} className="mt-1" />
+              <span>Copied!</span>
+            </button>
+          ) : (
+            <button
+              className="py-1 inline-flex items-center gap-1"
+              onClick={() => {
+                navigator.clipboard.writeText(code);
+                setCopy(true);
+                setTimeout(() => {
+                  setCopy(false);
+                }, 2000);
+              }}
+            >
+              <Clipboard size={16} className="mt-1" />
+              <span>Copy code</span>
+            </button>
           )}
         </div>
+        <SyntaxHighlighter
+          language="tsx"
+          style={github}
+          customStyle={{
+            padding: "25px",
+          }}
+          showLineNumbers
+        >
+          {code}
+        </SyntaxHighlighter>
       </div>
     </>
   );
