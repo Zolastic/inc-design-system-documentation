81468b9f3258aa86b72bd1f0cb6ea8fd1ef89754
Zolastic <honhattienlul@gmail.com>
2024-04-28 21:44:28 +0800

    chore: change indentation preview, usage and usage import code for Select component documentation

--- src/config/component-documentation/select.ts
+++ src/config/component-documentation/select.ts
@@ -54,48 +54,47 @@ const select: DocComponentDocumentation = {
   componentTitle: "Select",
   componentDescription: "Select component to select value from options.",
   previewCode: `import {
-              Select,
-              SelectContent,
-              SelectItem,
-              SelectTrigger,
-              SelectValue,
-            } from "inc-design-system/select";
-            import React from "react";
-            
-            const SelectDemo = () => {
-              return (
-                <Select>
-                  <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Theme" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="light">Light</SelectItem>
-                    <SelectItem value="dark">Dark</SelectItem>
-                    <SelectItem value="system">System</SelectItem>
-                  </SelectContent>
-                </Select>
-              );
-            };
-            
-            export default SelectDemo;`,
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "inc-design-system/select";
+  
+  const SelectDemo = () => {
+    return (
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+    );
+  };
+  
+  export default SelectDemo;`,
   previewComponent: SelectDemo,
   usageCodeImport: `import {
-          Select,
-          SelectContent,
-          SelectItem,
-          SelectTrigger,
-          SelectValue,
-        } from "inc-design-system/select";`,
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "inc-design-system/select"`,
   usageCode: `<Select>
-          <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Theme" />
-          </SelectTrigger>
-          <SelectContent>
-              <SelectItem value="light">Light</SelectItem>
-              <SelectItem value="dark">Dark</SelectItem>
-              <SelectItem value="system">System</SelectItem>
-          </SelectContent>
-      </Select>`,
+  <SelectTrigger className="w-[180px]">
+    <SelectValue placeholder="Theme" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectItem value="light">Light</SelectItem>
+    <SelectItem value="dark">Dark</SelectItem>
+    <SelectItem value="system">System</SelectItem>
+  </SelectContent>
+</Select>`,
   props: selectProps,
 };
 
