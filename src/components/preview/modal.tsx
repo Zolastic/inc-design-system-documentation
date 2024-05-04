e9eb2438c062ff73e9724d6b7c35adb3c4ca8560
Zolastic <honhattienlul@gmail.com>
2024-05-05 00:33:53 +0800

    chore: testing preview modal.tsx

--- src/components/preview/modal.tsx
+++ src/components/preview/modal.tsx
@@ -1,35 +1,36 @@
 import { Button } from "inc-design-system/button";
-import {
-  Modal,
-  ModalCloser,
-  ModalContent,
-  ModalTrigger,
-} from "inc-design-system/modal";
+// import {
+//   Modal,
+//   ModalCloser,
+//   ModalContent,
+//   ModalTrigger,
+// } from "inc-design-system/modal";
 import { Textarea } from "inc-design-system/textarea";
 import React from "react";
 
 const ModalDemo = () => {
   return (
-    <Modal>
-      <ModalTrigger>
-        <Button variant="dashed">Open Modal</Button>
-      </ModalTrigger>
-      <ModalContent
-        status="info"
-        title="Feedback"
-        description="Share your thoughts with us!"
-        className="w-96"
-        closable
-      >
-        <div className="text-text-default">
-          <h1 className="mb-1">What is your feedback?</h1>
-          <Textarea />
-        </div>
-        <ModalCloser className="mt-4">
-          <Button variant="primary">Confirm</Button>
-        </ModalCloser>
-      </ModalContent>
-    </Modal>
+    // <Modal>
+    //   <ModalTrigger>
+    //     <Button variant="dashed">Open Modal</Button>
+    //   </ModalTrigger>
+    //   <ModalContent
+    //     status="info"
+    //     title="Feedback"
+    //     description="Share your thoughts with us!"
+    //     className="w-96"
+    //     closable
+    //   >
+    //     <div className="text-text-default">
+    //       <h1 className="mb-1">What is your feedback?</h1>
+    //       <Textarea />
+    //     </div>
+    //     <ModalCloser className="mt-4">
+    //       <Button variant="primary">Confirm</Button>
+    //     </ModalCloser>
+    //   </ModalContent>
+    // </Modal>
+    <></>
   );
 };
 
