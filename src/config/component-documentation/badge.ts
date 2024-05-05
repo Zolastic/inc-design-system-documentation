cf4dafb8274f21969063e1078eb51f91fe80f0a6
Zolastic <honhattienlul@gmail.com>
2024-05-05 19:09:36 +0800

    fix: correct indentation misalignment for some components preview code
    - Components documentation affected: Alert Modal, Badge, Card, Modal and Popover

--- src/config/component-documentation/badge.ts
+++ src/config/component-documentation/badge.ts
@@ -71,26 +71,26 @@ import React from "react";
 const BadgeDemo = () => {
 return (
     <>
-    <Badge variant={"info"} count={1000}>
-        {/* to simulate an icon/ div */}
-        <div className="text-xl bg-gray-300 w-10 h-10 rounded flex justify-center items-center">
-        <Bell size={24} />
-        </div>
-    </Badge>
+      <Badge variant={"info"} count={1000}>
+          {/* to simulate an icon/ div */}
+          <div className="text-xl bg-gray-300 w-10 h-10 rounded flex justify-center items-center">
+          <Bell size={24} />
+          </div>
+      </Badge>
 
-    <Badge variant={"info"} dot>
-        {/* to simulate an icon/ div */}
-        <div className="text-xl bg-gray-300 w-10 h-10 rounded flex justify-center items-center">
-        <Bell size={24} />
-        </div>
-    </Badge>
+      <Badge variant={"info"} dot>
+          {/* to simulate an icon/ div */}
+          <div className="text-xl bg-gray-300 w-10 h-10 rounded flex justify-center items-center">
+          <Bell size={24} />
+          </div>
+      </Badge>
 
-    <Badge processing>
-        {/* to simulate an icon/ div */}
-        <div className="text-xl bg-gray-300 w-10 h-10 rounded flex justify-center items-center">
-        <Bell size={24} />
-        </div>
-    </Badge>
+      <Badge processing>
+          {/* to simulate an icon/ div */}
+          <div className="text-xl bg-gray-300 w-10 h-10 rounded flex justify-center items-center">
+          <Bell size={24} />
+          </div>
+      </Badge>
     </>
 );
 };
