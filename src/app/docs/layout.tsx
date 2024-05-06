273d4ec8379f9ce44e1980a137daba9f615a7395
Zolastic <honhattienlul@gmail.com>
2024-05-06 18:05:50 +0800

    style(docs/layout.tsx): Add min-h-screen class to docs layout for full-height content

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -20,7 +20,7 @@ export default function DocsLayout({ children }: DocsLayoutProps) {
   }
 
   return (
-    <div className="border-b bg-background text-text-default">
+    <div className="border-b bg-background text-text-default min-h-screen">
       <div className="container mx-auto flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
         <aside className="docsSidebarNavContainer fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-120px)] overflow-auto w-full shrink-0 md:sticky md:block">
           <ScrollArea className="h-full p-6 pr-6 lg:p-8">
