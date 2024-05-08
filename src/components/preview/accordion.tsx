01b97e53e8c3d385a8fe7055ab5ebb663e5e4532
Zolastic <honhattienlul@gmail.com>
2024-05-08 17:25:17 +0800

    refactor: refactor Accordion documentation to the updated Accordion

--- src/components/preview/accordion.tsx
+++ src/components/preview/accordion.tsx
@@ -1,26 +1,45 @@
-import Accordion from "inc-design-system/accordion";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "inc-design-system/accordion";
 import React from "react";
 
 const AccordionDemo = () => {
-  const headers: string[] = ["Header 1"];
-
-  const data = Array.from({ length: 1 }, (_, i) => ({
-    header: `accordion ${i + 1}`,
-    item: `content-${i + 1}`,
-  }));
+  type data = {
+    header: string;
+    content: string;
+  };
+  const accordionData: data[] = [
+    {
+      header: "Header 1",
+      content:
+        "Consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,",
+    },
+    {
+      header: "Header 2",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nisl tincidunt tincidunt",
+    },
+    {
+      header: "Header 3",
+      content: "This is some dummy content",
+    },
+  ];
 
   return (
-    <Accordion headers={headers}>
-      {data.map((item, index) => {
-        return (
-          <div key={index}>
-            <div>
-              <h1 className="text-lg font-bold">{item.header}</h1>
-              <p>{item.item}</p>
-            </div>
-          </div>
-        );
-      })}
+    <Accordion>
+      {accordionData.map((item, index) => (
+        <AccordionItem key={index} value={index.toString()}>
+          <AccordionTrigger>
+            <h1 className="text-lg font-bold">{item.header}</h1>
+          </AccordionTrigger>
+          <AccordionContent>
+            <p>{item.content} </p>
+          </AccordionContent>
+        </AccordionItem>
+      ))}
     </Accordion>
   );
 };
